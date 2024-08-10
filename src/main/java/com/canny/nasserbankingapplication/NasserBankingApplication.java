2c7bf139c13aec392348eed8332f2fb06dd53994
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-08-11 00:41:59 +0300

    initial commit

---
+++ src/main/java/com/canny/nasserbankingapplication/NasserBankingApplication.java
@@ -0,0 +1,13 @@
+package com.canny.nasserbankingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasserBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasserBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/canny/nasserbankingapplication/NasserBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canny.nasserbankingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasserBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
