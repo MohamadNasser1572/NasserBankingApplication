6ab4d35a72ee7d38dd8156e8bdcae527963b8e44
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-02-18 12:40:53 +0200

    refactor: add comment

--- src/main/java/com/canny/nasserbankingapplication/controller/AccountController.java
+++ src/main/java/com/canny/nasserbankingapplication/controller/AccountController.java
@@ -24,7 +24,7 @@ public class AccountController {
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     }
 
-    // Get Account REST API
+    // Get Account REST API   
     @GetMapping("/{id}")
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id) {
         AccountDto accountDto = accountService.getAccountById(id);
