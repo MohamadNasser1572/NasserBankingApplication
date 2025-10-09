1d5f4cd744795f8aa0d7431a5a62212522879089
Mohammad Nasser <mhmdnaser1572@gmail.com>
2025-10-09 08:46:32 +0300

    update account service updated

--- src/main/java/com/canny/nasserbankingapplication/service/AccountService.java
+++ src/main/java/com/canny/nasserbankingapplication/service/AccountService.java
@@ -2,17 +2,20 @@ package com.canny.nasserbankingapplication.service;
 
 import com.canny.nasserbankingapplication.dto.AccountDto;
 import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto account);
 
     AccountDto getAccountById(Long id);
 
-    AccountDto deposit(Long id,double amount);
+    AccountDto deposit(Long id, double amount);
 
-    AccountDto withdraw(Long id,double amount);
+    AccountDto withdraw(Long id, double amount);
 
     List<AccountDto> getAllAccounts();
 
     void deleteAccount(Long id);
+
+    AccountDto updateAccount(Long id, AccountDto accountDto);
 }
